bbd000ce27b6dbe0a67d09cedf3f6e1fca87fcdb
Mugiwara <talhaamirm@gmail.com>
2023-03-28 22:46:34 -0400

    added clickability to the project cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,10 +7,10 @@ import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
-const ProjectCard = ({index, name, description, tags, image, source_code_link}) => {
+const ProjectCard = ({index, name, url, description, tags, image, source_code_link}) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", 
-    index * 0.5, 0.75)}>
+    <a href={url} target={"_blank"}>
+      <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{
           max:45,
@@ -56,6 +56,7 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link}) 
         </div>
       </Tilt>
     </motion.div>
+    </a>  
   )
 }
 
@@ -76,15 +77,12 @@ const Works = () => {
           variants ={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          The following projects showcases my skills
-          and experience through
-          real-world examples of my work. Each
-          project is briefly described with
-          links to code repositories and live
-          demos in it. It reflects my
-          ability to solve complex problems, work 
-          with different technologies,
-          and manage projects effectively.
+          The following projects showcases my skillsand experience through real-world examples of my work. Each
+          project is briefly described with links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies, and manage projects effectively.
+          <p>&nbsp;</p>
+          Stay tuned for more upcoming projects!
+
         </motion.p>
       </div>
       <div className="mt-20 flex flex-wrap gap-7">
